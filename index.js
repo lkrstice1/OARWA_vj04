db1848c20d8ef743446b3956fd902839b71cfe31
Lucija Krsticevic <lkrstice1@pmfst.hr>
2023-05-12 21:36:45 +0200

    Dodane poruke na server

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Test")
+//ekvivalent naredbi import; http biblioteka koja je ugrađena u node.js
+const http = require('http')
+let poruke = [
+ {
+ id: 1,
+ sadrzaj: 'HTML je jednostavan',
+ },
+ {
+ id: 2,
+ sadrzaj: 'React koristi JSX sintaksu',
+ },
+ {
+ id: 3,
+ sadrzaj: 'GET i POST su najvaznije metode HTTP protokola',
+ }
+]
+const app = http.createServer((req, res) =>{
+ res.writeHead(200, {'Content-Type': 'application/json'})
+ res.end(JSON.stringify(poruke))
+})
+const PORT = 3001
+app.listen(PORT)
+console.log(`Posluzitelj je pokrenut na portu ${PORT}`);
